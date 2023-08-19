65431b794274f27e0bcb3cffeb4fdb4183676fe9
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-19 02:29:00 +0200

    added an interface called IPlant, having three abstract methods without a body, added two classes: Rose and Daisy, which implement the IPlant interface its abstract methods

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,1 +1,59 @@
 ﻿//learning interfaces 
+interface IPlant
+{
+    void planting();
+    void watering();
+    void selling();
+}
+
+class Rose : IPlant
+{
+    public string colour;
+    public double weigth;
+
+    public Rose(string colour, double weigth)
+    {
+        this.colour = colour;
+        this.weigth = weigth;
+    }
+
+    public void planting()
+    {
+
+    }
+
+    public void selling()
+    {
+        
+    }
+
+    public void watering()
+    {
+        
+    }
+}
+class Daisy : IPlant
+{
+    public int numberOfLeaves;
+    public string colour;
+
+    public Daisy(int numberOfLeaves, string colour)
+    {
+        this.numberOfLeaves = numberOfLeaves;
+        this.colour = colour;
+    }
+    public void planting()
+    {
+       
+    }
+
+    public void selling()
+    {
+       
+    }
+
+    public void watering()
+    {
+       
+    }
+}
